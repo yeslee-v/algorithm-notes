d6625223f959272fc41b3e9369d524459002f487
yeslee-v <yeslee5004@gmail.com>
2020-06-21 11:32:45 +0900

    Update 06-1차원 배열.py

--- Python/06-1차원 배열.py
+++ Python/06-1차원 배열.py
@@ -86,28 +86,20 @@ for j in ls:
     print(sum(ls_count))
 
 
+# 4344_평균은 넘겠지
 
+C = int(input())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for i in range(C):
+    score = list(map(int, input().split()))
+    avg = (sum(score)-score[0])/(len(score)-1)
+    count = 0;
+    
+    for j in range(score[0]):
+        if score[j+1] > avg:
+            count += 1
+            
+    upper_ratio = count * 100 / (len(score)-1)
+    print("%.3f%%"%upper_ratio)
 
 
