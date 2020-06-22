88883c488144ca00781c5945ee0a8c5feeb9c1d7
yeslee-v <yeslee5004@gmail.com>
2020-06-22 13:28:50 +0900

    Update 07-함수.py

--- Python/07-함수.py
+++ Python/07-함수.py
@@ -2,3 +2,18 @@
 
 def solve(n):
     return sum(n)
+
+
+# 4673_셀프 넘버
+
+self_num = set(range(1, 10001))
+generated_num = set()
+
+for i in range(1, 10001):
+    for j in str(i):
+        i = i + int(j)
+    generated_num.add(i)
+self_num = self_num - generated_num
+
+for k in sorted(self_num):
+    print(k)
