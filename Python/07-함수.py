5308382b87efdc2640a8092deaa757265a8b53ad
yeslee-v <yeslee5004@gmail.com>
2020-06-23 09:23:37 +0900

    solved 1065

--- Python/07-함수.py
+++ Python/07-함수.py
@@ -17,3 +17,30 @@ self_num = self_num - generated_num
 
 for k in sorted(self_num):
     print(k)
+
+
+# 1065_한수
+
+num = []; count = 0
+
+N = int(input())
+
+for i in range(1,N+1):      # 입력받은 전체 수
+    num.append(i)
+
+for j in range(len(num)):       # 하나씩 끊기
+    ls = []
+    
+    for k in str(num[j]):
+        ls.append(k)
+        
+    if len(ls) <=2:
+        count +=1
+        
+    else:
+        if int(ls[2]) - int(ls[1]) == int(ls[1]) - int(ls[0]):
+            count += 1
+
+print(count)    
+
+
