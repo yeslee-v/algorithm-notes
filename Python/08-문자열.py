d37bb299214f226c061c48809d67e291490c30be
yeslee-v <yeslee5004@gmail.com>
2020-06-26 11:08:32 +0900

    solved 10809

--- Python/08-문자열.py
+++ Python/08-문자열.py
@@ -12,3 +12,17 @@ answer = 0
 for i in str(num):
     answer += int(i)
 print(answer)
+
+
+# 10809_알파벳 찾기
+
+S = input(); ls = []
+
+for i in S:
+    ls.append(i)
+
+for m in range(ord('a'), ord('z') + 1):
+    if chr(m) in ls:
+        print(ls.index(chr(m)))
+    else:
+        print(-1)
