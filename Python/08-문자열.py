3c22c50511a9be89453115a3603fe9b9cc0e0497
yeslee-v <yeslee5004@gmail.com>
2020-06-27 11:50:57 +0900

    solved 2675

--- Python/08-문자열.py
+++ Python/08-문자열.py
@@ -26,3 +26,22 @@ for m in range(ord('a'), ord('z') + 1):
         print(ls.index(chr(m)))
     else:
         print(-1)
+
+
+# 2675_문자열 반복
+
+T = int(input())
+case = []
+
+for i in range(T):
+    R = input().split()
+    case.append(R)
+
+for m in range(T):
+    answer = []
+    for i in str(case[m][1]):
+        for j in range(1, int(case[m][0]) + 1):
+            result = i * j
+        answer.append(result)
+
+    print(''.join(answer))
