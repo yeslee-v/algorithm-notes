620320ff941038673790effece8e9a5af6c75cb6
yeslee-v <yeslee5004@gmail.com>
2020-06-28 10:34:05 +0900

    solved 1157

--- Python/08-문자열.py
+++ Python/08-문자열.py
@@ -1,47 +1,65 @@
-# 11654_아스키 코드
+# # 11654_아스키 코드
+#
+# n = input()
+# print(ord(n))
+#
+#
+# # 11720_숫자의 합
+#
+# N = int(input()); num = int(input())
+# answer = 0
+#
+# for i in str(num):
+#     answer += int(i)
+# print(answer)
+#
+#
+# # 10809_알파벳 찾기
+#
+# S = input(); ls = []
+#
+# for i in S:
+#     ls.append(i)
+#
+# for m in range(ord('a'), ord('z') + 1):
+#     if chr(m) in ls:
+#         print(ls.index(chr(m)))
+#     else:
+#         print(-1)
+#
+#
+# # 2675_문자열 반복
+#
+# T = int(input())
+# case = []
+#
+# for i in range(T):
+#     R = input().split()
+#     case.append(R)
+#
+# for m in range(T):
+#     answer = []
+#     for i in str(case[m][1]):
+#         for j in range(1, int(case[m][0]) + 1):
+#             result = i * j
+#         answer.append(result)
+#
+#     print(''.join(answer))
 
-n = input()
-print(ord(n))
 
+# 1157_단어 공부
 
-# 11720_숫자의 합
+count_words = []
 
-N = int(input()); num = int(input())
-answer = 0
+words = input().upper()
+ls_words = list(set(words))
+# print(words, ls_words, count_words)    # MISSISSIPI ['I', 'S', 'M', 'P'] []
 
-for i in str(num):
-    answer += int(i)
-print(answer)
+for i in ls_words:
+    count = words.count(i)  # 4 4 1 1
+    count_words.append(count)
 
-
-# 10809_알파벳 찾기
-
-S = input(); ls = []
-
-for i in S:
-    ls.append(i)
-
-for m in range(ord('a'), ord('z') + 1):
-    if chr(m) in ls:
-        print(ls.index(chr(m)))
-    else:
-        print(-1)
-
-
-# 2675_문자열 반복
-
-T = int(input())
-case = []
-
-for i in range(T):
-    R = input().split()
-    case.append(R)
-
-for m in range(T):
-    answer = []
-    for i in str(case[m][1]):
-        for j in range(1, int(case[m][0]) + 1):
-            result = i * j
-        answer.append(result)
-
-    print(''.join(answer))
+if (count_words.count(max(count_words))) >= 2:
+    print("?")
+else:
+    print(ls_words[count_words.index(max(count_words))])
