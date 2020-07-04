4eda6c56551674c8b16ba2cd4299b3e30cd75749
yeslee-v <yeslee5004@gmail.com>
2020-07-05 00:36:32 +0900

    solved 2839

--- Python/09-수학 1.py
+++ Python/09-수학 1.py
@@ -14,3 +14,24 @@ else:
 
 end = timeit.default_timer()
 print(end-start)
+
+
+# 2839_설탕 배달
+
+N = int(input())
+num_pack = 0
+
+while N > 0:
+    if N % 5 != 0:
+        N -= 3
+        if N < 0:
+            num_pack = -1
+            break
+        num_pack += 1
+    elif N % 5 == 0:
+        num_pack += 1
+        N -= 5
+    elif N % 5 != 0 and N % 3 != 0:
+        num_pack = -1
+
+print(num_pack)
