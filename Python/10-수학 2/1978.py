bea8dcd1878bfaf97f24e721b35d602176c7b526
yeslee-v <yeslee5004@gmail.com>
2020-07-05 22:18:30 +0900

    solved 1978:

--- Python/10-수학 2/1978.py
+++ Python/10-수학 2/1978.py
@@ -12,3 +12,27 @@ if len(num) == N:
             result += 1
 
 print(result)
+
+
+# Using Sieve of Eratosthenes
+
+import math
+
+N = int(input())
+num = list(map(int, input().split()))
+num_all = list(range(2, 1001))
+cnt = 0
+
+if len(num) == N:
+    for i in range(2, math.ceil(math.sqrt(1000))):  # math.ceil: 반올림 / math.sqrt: 제곱근
+        for j in num_all:
+            if j / i == 1:  # 같은 숫자는 패스
+                pass
+            elif j % i == 0:    # 그 외에도 나뉘는 수가 있으면 제거
+                num_all.remove(j)
+
+for k in num:
+    if k in num_all:
+        cnt += 1
+
+print(cnt)
