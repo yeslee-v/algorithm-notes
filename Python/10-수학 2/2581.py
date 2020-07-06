b2fa6b7347b450e8daa03287c38ae44c79fabb25
yeslee-v <yeslee5004@gmail.com>
2020-07-06 23:48:11 +0900

    solved 2581

---
+++ Python/10-수학 2/2581.py
@@ -0,0 +1,29 @@
+import math
+
+num = []
+sum = 0
+M = int(input())
+N = int(input())
+
+if M <= N:
+    for i in range(M, N + 1):
+        num.append(i)
+
+for i in range(2, math.ceil(math.sqrt(10000))):
+    for j in num:
+        if j / i == 1:  # 같은 숫자는 패스
+            pass
+        elif j % i == 0:
+            num.remove(j)
+
+if not num:  # 리스트가 비었을 때
+    print("-1")
+else:
+    for k in num:
+        sum += k
+    if 1 in num:  # 리스트 안에 1이 있는 경우
+        print(sum - 1)
+        print(num[1])
+    else:
+        print(sum)
+        print(num[0])
