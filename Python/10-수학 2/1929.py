4f022766bc6a2ceb7405aba4ef36fa84aef74be1
yeslee-v <yeslee5004@gmail.com>
2020-07-08 17:36:45 +0900

    solved 1929

---
+++ Python/10-수학 2/1929.py
@@ -0,0 +1,27 @@
+import math
+import timeit
+
+start = timeit.default_timer()
+
+sieve = []
+M, N = map(int, input().split())
+
+for i in range(M, N + 1):
+    sieve.append(i)
+
+if 1 in sieve:
+    sieve.remove(1)
+else:
+    for j in range(2, math.ceil(math.sqrt(sieve[-1]))):
+        for k in sieve:
+                if k / j == 1:
+                    pass
+                elif k % j == 0:
+                    sieve.remove(k)
+
+for answer in sieve:
+    print(answer)
+
+end = timeit.default_timer()
+
+print("time : ",end - start)
