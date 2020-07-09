c2574835b10a86bd8b1460915de2675b84a5dd27
yeslee-v <yeslee5004@gmail.com>
2020-07-09 23:58:26 +0900

    solve 4948

--- Python/10-수학 2/4948.py
+++ Python/10-수학 2/4948.py
@@ -1,17 +1,27 @@
-import math
-
+raw = []
 result = []
+
+
+def isPrime(a):
+    if 2 > a:
+        return False
+    for b in range(2, a):
+        if (a % b) == 0:
+            return False
+    return True
+
 
 while True:
     n = int(input())
     if n == 0:
         break
-    result.append(n)
+    raw.append(n)
 
-if 1 in result:
-    result.remove(1)
+for i in range(len(raw)):
+    result.append([])
+    for j in range((raw[i] + 1), (2 * raw[i]) + 1):
+        if isPrime(j):
+            result[i].append(j)
 
-for i in result:
-    for j in range(2, math.ceil(math.sqrt(result[-1]))+1):
-
-        print(i, j)
+for i in range(len(result)):
+    print(len(result[i]))
