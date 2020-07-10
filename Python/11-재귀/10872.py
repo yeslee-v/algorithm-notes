c9beb686a609cff2705a94510cdc8c559dd39b24
yeslee-v <yeslee5004@gmail.com>
2020-07-10 22:16:54 +0900

    update 10872

--- Python/11-재귀/10872.py
+++ Python/11-재귀/10872.py
@@ -1,16 +1,24 @@
 result = 1
-
 N = int(input())
 
 for i in range(1, N+1):
     result *= i
-
 print(result)
+
 
 # recursive function
 
 def func(n):
     return int(n <= 1) or n * func(n - 1)
 
-
 print(func(int(input())))
+
+
+# math function
+
+import math
+
+print(math.factorial(int(input())))
+
+
+
