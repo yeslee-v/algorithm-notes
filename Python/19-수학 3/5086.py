d744c22d8388ddf83cfec65afd763de11a11bb00
yeslee-v <yeslee5004@gmail.com>
2020-07-27 07:53:27 +0900

    solved 5086

---
+++ Python/19-수학 3/5086.py
@@ -0,0 +1,10 @@
+while 1:
+    x, y = list(map(int, input().split()))
+    if x == 0 and y == 0:
+        break
+    elif y % x == 0:
+        print('factor')
+    elif x % y == 0:
+        print('multiple')
+    else:
+        print('neither')
