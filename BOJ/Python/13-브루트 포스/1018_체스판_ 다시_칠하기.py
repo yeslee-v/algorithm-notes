dda9188730605cbc3d973ce8675dc2743601a025
Yeshin Lee <yeslee5004@gmail.com>
2022-08-23 00:05:33 +0900

    [Fix] 20220823

--- BOJ/Python/13-브루트 포스/1018_체스판_ 다시_칠하기.py
+++ BOJ/Python/13-브루트 포스/1018_체스판_ 다시_칠하기.py
@@ -1,17 +1,26 @@
 n, m = list(map(int, input().split(' ')))
 board = []
-i = 0
-res = 0
+res = []
 
-while (i < n):
+for _ in range(n):
     board.append(input())
-    i += 1
 
-for i in range(len(board) - 2):
-    for j in range(len(board) - 2):
-        if board[i][j] == board[i][j + 1] and board[i][j] != board[i][j + 2]:
-            res += 1
-    if board[i][j] == board[i + 1][j] and board[i][j] == board[i + 2][j]:
-        res += 1
+for i in range(n - 7):
+    for j in range(m - 7):
+        idx_w = 0
+        idx_b = 0
+        for k in range(i, i + 8):
+            for l in range(j, j + 8):
+                if (k + l) % 2 == 0:
+                    if board[k][l] != 'W':
+                        idx_w += 1
+                    if board[k][l] != 'B':
+                        idx_b += 1
+                else:
+                    if board[k][l] != 'B':
+                        idx_w += 1
+                    if board[k][l] != 'W':
+                        idx_b += 1
+        res.append(min(idx_w, idx_b))
 
-print(res)
+print(min(res))
