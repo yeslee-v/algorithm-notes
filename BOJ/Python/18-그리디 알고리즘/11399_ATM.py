4501963be7912ceeea2f0a9a4d08a161ed477485
Yeshin Lee <yeslee5004@gmail.com>
2022-09-26 10:31:09 +0900

    220926: insertion sort

--- BOJ/Python/18-그리디 알고리즘/11399_ATM.py
+++ BOJ/Python/18-그리디 알고리즘/11399_ATM.py
@@ -1,17 +1,27 @@
-import sys
-
-input = sys.stdin.readline
-
 n = int(input())
 arr = list(map(int, input().split()))
-answer = [0] * n
+result = [0] * n
+answer = 0
 
-arr.sort()
+for i in range(1, n):
+    insert_point = i
+    insert_value = arr[i]
+    for j in range(i - 1, -1, -1):
+        if arr[j] < arr[i]:
+            insert_point = j + 1
+            break
+        if j == 0:
+            insert_point = 0
+    for j in range(i, insert_point, -1):
+        arr[j] = arr[j - 1]
+    arr[insert_point] = insert_value
+
+result[0] = arr[0]
+
+for i in range(1, n):
+    result[i] = result[i - 1] + arr[i]
 
 for i in range(n):
-    result = 0
-    for j in range(i + 1):
-        result += arr[j]
-    answer[i] = result
+    answer += result[i]
 
-print(sum(answer))
+print(answer)
