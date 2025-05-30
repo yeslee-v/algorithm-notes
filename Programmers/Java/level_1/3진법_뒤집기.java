d7a9ef79fe2e08c624e44c8ae4a067014a0ece42
Yeshin Lee <yeslee5004@gmail.com>
2025-05-30 22:10:40 +0900

    2025-05-30

---
+++ Programmers/Java/level_1/3진법_뒤집기.java
@@ -0,0 +1,30 @@
+/*
+ * Math.pow(): 부동소수점 연산 사용해서 정밀도 떨어짐 -> 직접 연산하는 것이 좋다
+ * data type을 신경쓰자(max int)
+ */
+
+public int solution(int n) {
+    long tmp = 0, pow = 1;
+    int cnt = 0;
+
+    int quot = n;
+
+    while (0 < quot) {
+        tmp += (long) (quot % 3) * pow;
+
+        quot /= 3;
+        pow *= 10;
+        cnt++;
+    }
+
+    int answer = 0;
+    while (0 < cnt) {
+        cnt--;
+
+        answer += (tmp % 10) * Math.pow(3, cnt);
+
+        tmp /= 10;
+    }
+
+    return answer;
+}
